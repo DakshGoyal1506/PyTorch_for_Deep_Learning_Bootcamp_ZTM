51c79b140352f207e4e5d3d5289d9b240a8fc01c
Daksh Goyal <daksh.goyal1506@gmail.com>
2025-04-06 11:46:46 +0530

    Forgot to commit y progress

---
+++ 05/going_modular/train.py
@@ -0,0 +1,50 @@
+"""
+Trains a PyTorch image classification model using device-agnostic code.
+"""
+import os
+import torch
+from torchvision import transforms
+from torch import  nn
+import data_setup, engine, model_builder, utils
+
+NUM_EPOCHS = 5
+BATCH_SIZE = 32
+HIDDEN_UNITS = 10
+LEARNING_RATE = 0.001
+
+train_dir = "../data/pizza_steak_sushi/train"
+test_dir = "../data/pizza_steak_sushi/test"
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
+
+data_transform = transforms.Compose([
+    transforms.Resize(size=[64, 64]),
+    transforms.ToTensor()
+])
+
+train_dataloader, test_dataloader, class_names = data_setup.create_dataloaders(
+    train_dir=train_dir,
+    test_dir=test_dir,
+    transform=data_transform,
+    batch_size=BATCH_SIZE,
+    num_workers=1)
+
+model = model_builder.TinyVGG(input_shape=3,
+                              hidden_units=10,
+                              output_shape=len(class_names)).to(device)
+
+loss_fn = nn.CrossEntropyLoss()
+optimizer = torch.optim.Adam(params=model.parameters(),
+                             lr=LEARNING_RATE)
+
+results = engine.train(model=model,
+                       train_dataloader=train_dataloader,
+                       test_dataloader=test_dataloader,
+                       optimizer=optimizer,
+                       loss_fn=loss_fn,
+                       epochs=NUM_EPOCHS,
+                       device=device)
+
+utils.save_model(model=model,
+                 target_dir="models",
+                 model_name="05_going_modular_script_mode_tinyvgg_model.pth")
